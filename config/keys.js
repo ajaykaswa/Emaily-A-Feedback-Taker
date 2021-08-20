bd4907962c8d5eb8f8062789dda972292731de9d
ajaykaswa <ajaykaswa87@gmail.com>
2021-08-20 15:36:46 +0530

    Finished OAuth

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+//keys.js figure out
+
+if(process.env.NODE_ENV=='production'){
+//we are in production
+ module.exports= require('./prod');
+}else{
+//we are in development
+module.exports = require('./dev');
+
+}
+
